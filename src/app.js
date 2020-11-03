a9e5b024b0bcf1361514fdc6db8a034910b75720
Muneeza Nasir <muneezanasir24@gmail.com>
2020-11-03 20:14:12 +0500

    small changes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const response  = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || '3000';
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
