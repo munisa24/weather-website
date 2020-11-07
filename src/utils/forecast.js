a982f1d7af1fdc2c9e92be9c7c3cf8211bac20cd
Muneeza Nasir <muneezanasir24@gmail.com>
2020-11-07 14:43:10 +0500

    some changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,5 @@
 const request = require('request');
 
-
-
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=93e5524ace5d9b835e5f3e8e835839fd&query=' + latitude + ' , ' + longitude +'&units=m';
 
